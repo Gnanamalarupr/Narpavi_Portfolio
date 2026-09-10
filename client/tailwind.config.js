135960810c284cd13bdbe70c73932cfdf8b52412
Gnanamalarupr <gnanamalarpalanisamy@gmail.com>
2026-09-10 16:10:33 +0530

    Built frontend configuration

---
+++ client/tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+export default { content:['./index.html','./src/**/*.{js,jsx}'], darkMode:'class', theme:{extend:{colors:{ink:'#382d2e',rose:'#b8757e',blush:'#f8e9e6',cream:'#fffaf5',gold:'#ba9870'},fontFamily:{display:['Playfair Display','serif'],sans:['DM Sans','sans-serif']},boxShadow:{soft:'0 16px 50px rgba(89,55,47,.12)'}}},plugins:[] };
